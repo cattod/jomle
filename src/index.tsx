00cfe7fba4e24a134cadb5998f2361b84da4a926
hamidreza <hr.kazemi11@gmail.com>
2020-04-13 17:24:26 +0430

    useless files remvoed

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './asset/style/app/skin-default/style.scss';
+import { App } from './component/app/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { persistor, Store2 } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={Store2}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
